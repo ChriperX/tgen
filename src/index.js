37aacd66d70154a1e63c6d1081895daec12a3bfb
Christian <nonamereal@libero.it>
2020-03-11 14:51:55 +0100

    Added plugin support for commands, docs needed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-//TODO
-const t = require('./templates');
-t.templates();
+#!/usr/bin/env node
+const template = require('./templates');
+const tp = require('@nonamenpm/text-parser');
+const fs = require('fs');
+const plugger = require('@nonamenpm/plugger');
+
+//logLevel for logger
+let logLevel = '';
+let plugins = [];
+
+//#region PLUGIN_ENTRY_POINT
+
+//entry point for plugins
+exports.commands = {};
+
+exports.use = function(plugin) {
+	plugger(plugin, this, false);
+};
+
+function addCustomCommands() {
+	for (key in exports.commands) {
+		tp.add(exports.commands[key]['command'], exports.commands[key]['cb'], exports.commands[key]['desc']);
+	}
+}
+
+function walk(dir) {
+	files = fs.readdirSync(dir);
+	files.forEach((element) => {
+		plugins.push(require(dir + element));
+	});
+}
+
+walk('../plugins/parser/');
+exports.use(plugins);
+addCustomCommands();
+
+//#endregion PLUGIN_ENTRY_POINT
+
+//add the new command, with template and name
+tp.add('new <template> <name>', (element) => {
+	template.loadTemplates(element, logLevel);
+});
+
+tp.add('-v --verbose', () => {
+	logLevel = 'verbose';
+});
+
+tp.parse();
