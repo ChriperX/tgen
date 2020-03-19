d7edcc485cc58cd103c11df13767ac7aa1566ba5
some_guy_oninternet <nonamereal@libero.it>
2020-03-19 18:43:52 +0100

    Added test for mem.js

---
+++ test/mem.js
@@ -0,0 +1,22 @@
+const mem = require('../src/utils/mem.js');
+const expect = require('chai').expect;
+
+describe('mem', () => {
+	describe('newVar()', () => {
+		it('should return contents', () => {
+			expect(mem.newVar('new var', 'test var')).to.be.equal('new var');
+		});
+	});
+
+	describe('fetch()', () => {
+		it('should return test var', () => {
+			expect(mem.fetch('test var')).to.be.equal('new var');
+		});
+	});
+
+	describe('replaceVars()', () => {
+		it('should return the string with vars', () => {
+			expect(mem.replaceVars('this (is) a (test var)')).to.be.equal('this (is) a new var');
+		});
+	});
+});
