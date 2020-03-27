41cb83fa95de440f658888be89215e7e9215d6d8
some_guy_oninternet <nonamereal@libero.it>
2020-03-27 19:43:44 +0100

    Example file

---
+++ plugins/messages/messages.examples.js
@@ -0,0 +1,9 @@
+//this is an example file for modifying the messages in tgen
+//uncomment to see what the example does
+/*
+exports.logMessages = {
+	error: {
+		yaml_bad_formatting: chalk.cyanBright('This message was modified by the example file.')
+	}
+};
+*/
