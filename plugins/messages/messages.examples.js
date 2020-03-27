715bbc8aa7ba75c09128d5219f2e69667a6b30d5
some_guy_oninternet <nonamereal@libero.it>
2020-03-27 22:17:41 +0100

    Fixed example

--- plugins/messages/messages.examples.js
+++ plugins/messages/messages.examples.js
@@ -1,9 +1,11 @@
 //this is an example file for modifying the messages in tgen
 //uncomment to see what the example does
 /*
+const chalk = require('chalk');
+
 exports.logMessages = {
 	error: {
-		yaml_bad_formatting: chalk.cyanBright('This message was modified by the example file.')
+		template_not_found: chalk.cyanBright('This message was modified by the example file. ')
 	}
 };
 */
